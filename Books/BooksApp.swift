08687d5363603031e247500efc61996f09403aea
Ahmed <ahmed.sultan@edu.uekat.pl>
2026-04-05 14:22:41 +0200

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Ahmed Sultan on 05/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
